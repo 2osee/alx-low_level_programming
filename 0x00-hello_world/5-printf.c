0e7fdc494cb26c29153edd77fce87b693d29c2d8
2osee <yangandeosee2@gmail.com>
2022-04-10 13:33:28 -0700

    Sha wanduhije kbs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-*main -Entry point
-*
-*Return: Always 0 (Success)
-*/
-int main(void)
+* main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
 {
-/*function printing a srting */
-printf("with proper grammar,but the outcome is a piece of art,");
+/* function printing a string */
+printf("with proper grammar,  but the outcome is a piece of art, ");
 return (0);
 }
