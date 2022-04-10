c20abf36a534eae514b03433ce4228c17fc121c1
2osee <yangandeosee2@gmail.com>
2022-04-10 13:00:53 -0700

    The last code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
-* * main - prints exactly "and that piece of art is useful"
-*  * Return: Always 0 (Success)
-*/
-int main(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
 {
-              write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
-                      return (1);
+/* function printing a string */
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
