e68535dddbb4014ce2aa963d6354ca8f822bc2ec
2osee <yangandeosee2@gmail.com>
2022-04-10 09:08:50 -0700

    FInal Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-/* main function */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void) 
 {
 /* function printing a string */
