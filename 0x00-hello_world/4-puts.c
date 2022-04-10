0d96f25f08af5e259225766b9ff692bf49e8b69b
2osee <yangandeosee2@gmail.com>
2022-04-10 08:19:39 -0700

    Fix: Compilation Error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
-/**
-*main - entry point
-*
-*return: o after printing the function
-*/
-int main(void)
-{
-puts("\"programming is like building a multingual puzzle");
-return (0);
+int main() {
+   printf("Hello, World!");
+   return 0;
 }
