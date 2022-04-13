e6591685aec8a4464c3713c86ed8de72dd6f8ab8
2osee <yangandeosee2@gmail.com>
2022-04-13 15:21:41 -0700

    Review

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,33 +6,27 @@
  *main - The containing function of the c programme
  *Return: will return 0 when sucessful
 */
- 
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	
 	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
-	
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	
 	else
 	{
 		printf("%d is error\n", n);
 	}
-	
 	return (0);
 }
